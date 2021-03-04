4e1cd31e22a5688c17ea99274f1cd990ff9ff8e0
Sandhiya Natarajan <vtu7514@veltechuniv.edu.in>
2021-03-04 17:42:36 +0530

    Create add a Gig

---
+++ SocialNetwork/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using SocialNetwork.Models;
+using SocialNetwork.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    public class GigsController : Controller
+
+    {
+
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+        public ActionResult Create()
+        {
+            var viewmodel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewmodel);
+        }
+    }
+}
---
+++ SocialNetwork/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetwork.Models;
+using System.Collections.Generic;
+
+namespace SocialNetwork.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int  Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
